fd2329eb941cfde4fae77f57b7dff38ad18a04bb
Felipe Badaró <felipe_badaro@yahoo.com.br>
2019-11-06 10:27:43 -0300

    Limpando o index

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
-<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
-   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Leaflet e Openstreetmap</title>
    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/leaflet.css" />
    <link rel="stylesheet" type="text/css" href="css/style.css" />
 
-   <script
-   src="https://code.jquery.com/jquery-3.4.1.min.js"
-   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-   crossorigin="anonymous"></script>
-   <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/leaflet.js'></script>
+   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/leaflet.js"></script>
 </head>
-
 <body>
+
    <p>
       <b>Demo de mapa com leaf e openstreetmap</b>
    </p>
+   
+   <div id="map"></div>   
 
-      <div id="map"></div>
-
-      <script type='text/javascript' src='js/markers.json'></script>
-      <script type='text/javascript' src='js/leaf-demo.js'></script>
-   </body>
-   </html>
+   <script type='text/javascript' src='js/markers.json'></script>
+   <script type='text/javascript' src='js/leaf-demo.js'></script>
+</body>
+</html>
