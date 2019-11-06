e8ac3ca675c50a598cfa0923dcbeb042e4d11774
Felipe Badaró <felipe_badaro@yahoo.com.br>
2019-11-06 10:22:57 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+<head profile="http://gmpg.org/xfn/11">
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/leaflet.css" />
+   <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+   <script
+   src="https://code.jquery.com/jquery-3.4.1.min.js"
+   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+   crossorigin="anonymous"></script>
+   <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/leaflet.js'></script>
+</head>
+
+<body>
+   <p>
+      <b>Demo de mapa com leaf e openstreetmap</b>
+   </p>
+
+      <div id="map"></div>
+
+      <script type='text/javascript' src='js/markers.json'></script>
+      <script type='text/javascript' src='js/leaf-demo.js'></script>
+   </body>
+   </html>
---
+++ js/leaf-demo.js
@@ -0,0 +1,26 @@
+//Inicializacao, visao e zoom inicial
+var map = L.map( 'map', {
+    center: [-22.918373, -43.212655],
+    minZoom: 2,
+    zoom: 16
+});
+
+
+//mapa de fundo
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo( map );
+
+
+//Criando um grupo para os marcadores
+var grupo = new L.featureGroup();
+for (var i = 0; i < marcadores.length; i++) {
+    L.marker([marcadores[i].lat, marcadores[i].lng]).addTo(grupo);
+}
+
+// Adicinando o grupo ao mapa
+grupo.addTo(map);
+
+//Calculando limites e dando padding nos pontos, para ficarem visíveis
+map.fitBounds(grupo.getBounds().pad(0.1));
